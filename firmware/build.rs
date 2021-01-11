162c4ea059192f3f7fe9a90284a1ea204a04c1d5
Matt Mullins <mmullins@mmlx.us>
2021-01-10 23:34:43 -0800

    firmware: run the 6502 assembler from build.rs
    This way I don't have to remember how to invoke `xa` every time.  And I
    also can't forget *to* run it.

---
+++ firmware/build.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+use std::process::{exit, Command};
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/hello_world.S");
+
+    let out_dir: PathBuf = std::env::var("OUT_DIR")
+        .expect("OUT_DIR should be set")
+        .into();
+
+    dbg!(&out_dir);
+
+    let mut child = Command::new("xa")
+        .args(&[
+            "-C",
+            "src/hello_world.S",
+            "-o",
+            out_dir
+                .join("hello_world.bin")
+                .to_str()
+                .expect("path should be valid UTF-8"),
+        ])
+        .spawn()
+        .expect("could not run the xa assembler");
+    let status = child.wait().expect("child should have succeeded");
+    exit(
+        status
+            .code()
+            .expect("process did not result in an exit code"),
+    );
+}
